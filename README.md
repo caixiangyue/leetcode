6b48d56a306ae93ad84efabae7dad6a99126a3d6
caixiangyue <caixiangyue007@gmail.com>
2019-07-02 17:03:27 +0800

    add Same Tree C solution

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 | # | Title | Solution | Difficulty |
 | :------ | :------ | :------ | :----- |
+| 100 | [Same Tree](https://leetcode.com/problems/same-tree/) | [C](./c/SameTree/same_tree.c)  | Easy |
 | 101 | [Symmetric Tree](https://leetcode.com/problems/symmetric-tree/) | [C](./c/SymmetricTree/symmetric_tree.c)  | Easy |
---
+++ c/SameTree/same_tree.c
@@ -0,0 +1,25 @@
+/**
+ * Author: Xiangyue Cai
+ * Date: 2019-07-02
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+    if (!p && !q)
+        return true;
+    if (!p || !q)
+        return false;
+    if(p->val != q->val)
+        return false;
+    return isSameTree(p->left, q->left) &&
+        isSameTree(p->right, q->right);
+}
