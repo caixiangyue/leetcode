31d25099b6980a52edc335affe8661d0d62e84e2
Ylq-127 <1012617115@qq.com>
2019-09-11 14:32:14 +0800

    今年的第几天

---
+++ cc/N34.今年的第几天/今年的第几天.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int Y,M,D;
+    cin >> Y >> M >> D;
+    int day=0;
+    int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    if((Y%4==0 && Y%100!=0)|| Y%400==0){//≈–∂œ»ÚƒÍ
+        if(M<=2) day=month[0];
+        else{
+            for(int i=0;i<M-1;i++){
+                day += month[i];
+            }
+            day++;
+        }
+    }else{
+        for(int i=0;i<M-1;i++) day += month[i];
+    }
+    day += D;
+    cout << day << endl;
+    return 0;
+}
